de63c2c66bd6da4652b0581657d5623db4e32bab
xiaoyouming <739243573@qq.com>
2020-04-23 19:16:46 +0800

    （*） fix 定时器

--- src/main/java/auth/AuthApplication.java
+++ src/main/java/auth/AuthApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author xiaoyouming
  * @version 1.0
  * @since 2020-04-15
  */
-
+@EnableScheduling
 @EnableEurekaClient
 @SpringBootApplication
 @MapperScan(basePackages = "auth.dao")
